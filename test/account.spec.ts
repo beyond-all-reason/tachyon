85849322fd73fd8323dc3d7387665d78f6bd6910
Jazcash <jaspercashmore@gmail.com>
2023-03-19 12:05:17 +0000

    wip

--- test/account.spec.ts
+++ test/account.spec.ts
@@ -1,13 +1,16 @@
+import { Type } from "@sinclair/typebox";
 import Ajv from "ajv";
 
-import schema from "../schema.json";
+import schema from "../schema/full_1.json";
 
-const ajv = new Ajv();
-
-const accountEndpoints = schema.endpoints.account;
+const ajv = new Ajv({
+    schemas: {
+        schema["$defs"]
+    }
+});
 
 test("who_am_i:request", () => {
-    const requestValidator = ajv.compile(accountEndpoints.who_am_i.request);
+    const requestValidator = ajv.compile(accountEndpoints.who_am_i.properties.request);
     const valid = requestValidator({});
     if (!valid) {
         console.log(requestValidator.errors);
@@ -16,6 +19,12 @@ test("who_am_i:request", () => {
 });
 
 test("who_am_i:response", () => {
-    // TODO
-    expect(true).toBe(true);
+    const responseValidator = ajv.compile(accountEndpoints.who_am_i.properties.response);
+    const valid = responseValidator({
+        name: "bob"
+    });
+    if (!valid) {
+        console.log(responseValidator.errors);
+    }
+    expect(valid).toBe(true);
 });
