2991126ed9f8bc6a8680f10b28826373868b3ea1
Jazcash <jaspercashmore@gmail.com>
2023-06-18 15:34:11 +0100

    removed dummy response

--- src/schema/init.d.ts
+++ src/schema/init.d.ts
@@ -2,16 +2,11 @@ import { DefineServiceSchema } from "../helpers";
 
 export type InitService = DefineServiceSchema<{
     init: {
-        response:
-            | {
-                  status: "success";
-                  data: {
-                      tachyonVersion: string;
-                  };
-              }
-            | {
-                  status: "failed";
-                  reason: "shit";
-              };
+        response: {
+            status: "success";
+            data: {
+                tachyonVersion: string;
+            };
+        };
     };
 }>;
