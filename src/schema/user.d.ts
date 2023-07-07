4f4a1b7ac1bbf3fe4aa3e7f30d59960bcdfca1c7
Jazcash <jaspercashmore@gmail.com>
2023-07-07 21:20:52 +0100

    delete

--- src/schema/user.d.ts
+++
@@ -1,9 +0,0 @@
-import { DefineServiceSchema, EmptyObject } from "../helpers";
-
-export type LobbyService = DefineServiceSchema<{
-  // This is the server telling you your userClient has been updated
-  updateStatus: {
-    request: EmptyObject;
-    response: { success: EmptyObject };
-  };
-}>;
