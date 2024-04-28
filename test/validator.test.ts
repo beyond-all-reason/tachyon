b50215fb9611ee0ad8a73f340a5ef3f61da55bdb
Jazcash <jaspercashmore@gmail.com>
2024-04-28 03:24:22 +0100

    more tests

---
+++ test/validator.test.ts
@@ -0,0 +1,81 @@
+/* eslint-disable no-restricted-imports */
+
+import { MatchmakingQueueRequest, MatchmakingQueueResponse } from "../dist";
+import validators from "../dist/validators";
+
+describe("request", () => {
+    const validator = validators.matchmaking_queue_request;
+
+    test("valid", () => {
+        const command: MatchmakingQueueRequest = {
+            commandId: "matchmaking/queue/request",
+            messageId: "123",
+            data: {
+                queues: ["1v1"],
+            },
+        };
+
+        const isValid = validator(command);
+
+        expect(isValid).toBe(true);
+
+        if (isValid) {
+            expect(command.data.queues).toStrictEqual(["1v1"]);
+        }
+    });
+
+    test("invalid", () => {
+        const command: MatchmakingQueueRequest = {
+            commandId: "matchmaking/queue/request",
+            messageId: "123",
+            data: {
+                // @ts-expect-error
+                queues: [],
+            },
+        };
+
+        const isValid = validator(command);
+
+        expect(isValid).toBe(false);
+
+        if (!isValid) {
+            expect(validator.errors).toBeTruthy();
+            expect(validator.errors?.length).toBeGreaterThan(0);
+        }
+    });
+});
+
+describe("response", () => {
+    const validator = validators.matchmaking_queue_response;
+
+    test("valid", () => {
+        const command: MatchmakingQueueResponse = {
+            commandId: "matchmaking/queue/response",
+            messageId: "56023780-bf7c-48a4-b2c7-a9eac00e7249",
+            status: "failed",
+            reason: "command_unimplemented",
+        };
+
+        const isValid = validator(command);
+
+        expect(isValid).toBe(true);
+    });
+
+    test("invalid", () => {
+        const command: MatchmakingQueueResponse = {
+            commandId: "matchmaking/queue/response",
+            // @ts-expect-error
+            messageId: 123,
+            status: "success",
+        };
+
+        const isValid = validator(command);
+
+        expect(isValid).toBe(false);
+
+        if (!isValid) {
+            expect(validator.errors).toBeTruthy();
+            expect(validator.errors?.length).toBeGreaterThan(0);
+        }
+    });
+});
