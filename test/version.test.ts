67546de497921378acd9b6413f83b090bc3c6fb6
Jazcash <jaspercashmore@gmail.com>
2024-06-17 13:40:27 +0100

    swap from jest to native node test runner, test cjs and esm separately

--- test/version.test.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-restricted-imports */
-
-import { tachyonMeta } from "../dist";
-import packageJson from "../package.json";
-
-test("version", () => {
-    expect(tachyonMeta.version).toEqual(packageJson.version);
-});
