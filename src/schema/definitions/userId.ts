65a2445e74028a572e7728b521d5582fe262969a
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2024-11-18 07:34:36 +0000

    Remove formating constraint on user id
    No need to require a UUID, it's treated as an opaque string

--- src/schema/definitions/userId.ts
+++ src/schema/definitions/userId.ts
@@ -2,6 +2,5 @@ import { Type } from "@sinclair/typebox";
 
 export const userId = Type.String({
     $id: "userId",
-    format: "uuid",
-    examples: ["f47a7e1e-4b2f-4d3d-3f3c-1f0f0e4b7e1e"],
+    examples: ["351"],
 });
