59e42971fd94b11084ce329d5fd2eeb2fa9c72c5
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2025-03-08 07:25:20 +0000

    Add a matchmaking event for parties
    Because there is now a way for a player to be added to matchmaking
    without him doing anything.

---
+++ src/schema/matchmaking/queuesJoined.ts
@@ -0,0 +1,15 @@
+import { Type } from "@sinclair/typebox";
+
+import { defineEndpoint } from "@/generator-helpers.js";
+
+export default defineEndpoint({
+    source: "server",
+    target: "user",
+    description:
+        "Indicate the player has been added to some queues by someone else. This happens in a party settings.",
+    event: {
+        data: Type.Object({
+            queues: Type.Array(Type.String(), { minItems: 1 }),
+        }),
+    },
+});
