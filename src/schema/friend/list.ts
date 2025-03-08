f6de0e742496be711ad734e70dfefd6bc8fafa12
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2025-03-08 15:44:04 +0000

    Fix friend/list
    Request and response were swapped

--- src/schema/friend/list.ts
+++ src/schema/friend/list.ts
@@ -8,27 +8,30 @@ export default defineEndpoint({
     source: "user",
     target: "server",
     description: "Retrieve the status of your friendlist",
-    request: {
-        data: Type.Object({
-            friends: Type.Array(
-                Type.Object({
-                    userId: Type.Ref(userId),
-                    addedAt: Type.Ref(unixTime),
-                })
-            ),
-            outgoingPendingRequests: Type.Array(
-                Type.Object({
-                    to: Type.Ref(userId),
-                    sentAt: Type.Ref(unixTime),
-                })
-            ),
-            incomingPendingRequests: Type.Array(
-                Type.Object({
-                    from: Type.Ref(userId),
-                    sentAt: Type.Ref(unixTime),
-                })
-            ),
-        }),
-    },
-    response: [{ status: "success" }],
+    request: {},
+    response: [
+        {
+            status: "success",
+            data: Type.Object({
+                friends: Type.Array(
+                    Type.Object({
+                        userId: Type.Ref(userId),
+                        addedAt: Type.Ref(unixTime),
+                    })
+                ),
+                outgoingPendingRequests: Type.Array(
+                    Type.Object({
+                        to: Type.Ref(userId),
+                        sentAt: Type.Ref(unixTime),
+                    })
+                ),
+                incomingPendingRequests: Type.Array(
+                    Type.Object({
+                        from: Type.Ref(userId),
+                        sentAt: Type.Ref(unixTime),
+                    })
+                ),
+            }),
+        },
+    ],
 });
