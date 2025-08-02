36c9841cbb6620a80b010db1944900ceedea1672
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2025-08-02 19:23:19 +0200

    Add player skill (#61)
    Address https://github.com/beyond-all-reason/tachyon/issues/58
    
    Only skill for now, if we want to add more data like `provistional` or
    whatever, we'll see when we know what we want exactly.

--- src/schema/definitions/user.ts
+++ src/schema/definitions/user.ts
@@ -12,6 +12,13 @@ export const user = Type.Object(
         clanId: Nullable(Type.String()),
         countryCode: Type.Optional(Type.String()),
         status: UnionEnum(["offline", "menu", "playing", "lobby"]),
+        rating: Type.Optional(
+            Type.Object({
+                value: Type.Number({
+                    description: "Key is omitted when the player isn't rated yet.",
+                }),
+            })
+        ),
     },
     { $id: "user" }
 );
