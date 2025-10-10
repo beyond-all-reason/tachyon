3bc9438b106b3ba0be788fc99fe4d329d05d8ee0
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2025-10-10 15:48:26 +0100

    Add basics for bots supports

---
+++ src/schema/lobby/removeBot.ts
@@ -0,0 +1,19 @@
+import { Type } from "@sinclair/typebox";
+
+import { defineEndpoint } from "@/generator-helpers.js";
+
+export default defineEndpoint({
+    source: "user",
+    target: "server",
+    description: "Remove the specified bot from the lobby",
+    request: {
+        data: Type.Object({
+            id: Type.String(),
+        }),
+    },
+    response: [
+        { status: "failed", reason: "not_in_lobby" },
+        { status: "failed", reason: "invalid_bot" },
+        { status: "success" },
+    ],
+});
