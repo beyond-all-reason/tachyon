37a87acf9e4d3f7d60c4ee74d573b5a6d7c2ea2d
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2025-10-25 16:11:40 +0100

    Fix allyTeamConfig definition
    The previous one had one too many layer.

--- src/schema/definitions/allyTeamConfig.ts
+++ src/schema/definitions/allyTeamConfig.ts
@@ -1,22 +1,17 @@
 import { Type } from "@sinclair/typebox";
 
-export const allyTeamConfig = Type.Object(
-    {
-        allyTeamConfig: Type.Array(
-            Type.Object(
-                {
-                    maxTeams: Type.Integer({ minimum: 1 }),
-                    startBox: Type.Ref("startBox"),
-                    teams: Type.Array(
-                        Type.Object({
-                            maxPlayers: Type.Integer({ minimum: 1 }),
-                        })
-                    ),
-                },
-                { description: "config for this ally team" }
+export const allyTeamConfig = Type.Array(
+    Type.Object(
+        {
+            maxTeams: Type.Integer({ minimum: 1 }),
+            startBox: Type.Ref("startBox"),
+            teams: Type.Array(
+                Type.Object({
+                    maxPlayers: Type.Integer({ minimum: 1 }),
+                })
             ),
-            { description: "each object describes an ally team" }
-        ),
-    },
-    { $id: "allyTeamConfig" }
+        },
+        { description: "config for this ally team" }
+    ),
+    { description: "each object describes an ally team", $id: "allyTeamConfig" }
 );
