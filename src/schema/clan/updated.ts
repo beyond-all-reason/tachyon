dd11ac8497335d67d215d1c75eb5638fdc092b19
Rimek86 <Rimek86@t-online.de>
2026-01-25 09:27:59 +0100

    Comments removed

--- src/schema/clan/updated.ts
+++ src/schema/clan/updated.ts
@@ -8,9 +8,9 @@ export default defineEndpoint({
     description: "One ore more clan properties were updated. Sent to all clan members.",
     event: {
         data: Type.Object({
-            description: Type.Optional(Type.String({ maxLength: 500 })), // new description
-            name: Type.Optional(Type.String({ maxLength: 30 })), // new name
-            tag: Type.Optional(Type.String({ minLength: 3, maxLength: 6 })), // new tag
+            description: Type.Optional(Type.String({ maxLength: 500 })),
+            name: Type.Optional(Type.String({ maxLength: 30 })),
+            tag: Type.Optional(Type.String({ minLength: 3, maxLength: 6 })),
         }),
     },
 });
