ae1d7782182991ca952523576a2149a4eda02fff
Lexon <44340857+L-e-x-o-n@users.noreply.github.com>
2026-09-22 09:21:42 +0200

    Majority as percentage of votes (#158)
    Redefines majority as percentage of cast votes instead of being a
    specific number of votes.
    
    Co-authored-by: Teifion <teifion@teifion.co.uk>

--- src/schema/definitions/lobbyDetails.ts
+++ src/schema/definitions/lobbyDetails.ts
@@ -101,11 +101,13 @@ export const lobbyDetails = Type.Object(
                 until: Type.Ref("unixTime"),
                 quorum: Type.Integer({
                     minimum: 1,
-                    description: "this many player must vote for the vote to be valid at all.",
+                    description: "this many player must vote for the vote to be valid at all",
                 }),
-                majority: Type.Integer({
-                    minimum: 1,
-                    description: "votes passes when number(yes) >= majority",
+                majority: Type.Number({
+                    minimum: 0,
+                    maximum: 1,
+                    description:
+                        "percentage of yes votes out of non abstaining votes required for the vote to pass",
                 }),
             })
         ),
